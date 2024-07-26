423145e81f04147a383478c6712d8a69f10b72c2
famas11 <blomkvistlars@hotmail.com>
2024-07-26 10:50:59 +0200

    Added get book text function

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
-print("hello world")
+def main():
+    book_path = "books/frankenstein.txt"
+    book_text = get_book_text(book_path)
+    print(book_text)
+
+def get_book_text(path):
+    with open(path) as f:
+        return f.read()
+
+
+
+main()
